d963ba73db7e18d91368e2ae9493c2dc848ff9e9
Michał Obrębski <mobrebski04@gmail.com>
2026-05-12 16:15:18 +0200

    Create MainWindow.xaml.cs

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+using System.Windows;
+
+namespace WpfApp // Upewnij się, że to pasuje do Twojego projektu
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
